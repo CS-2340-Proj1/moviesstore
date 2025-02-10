8c556b8cb0f5f973f4a7b9948bb1786abd5e8250
Hypersomnia1 <harryfei04@gmail.com>
2025-02-10 03:50:28 -0500

    password reset

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,9 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
-from django.contrib.auth.forms import PasswordResetForm
-import logging
 
-logger = logging.getLogger(__name__)
-
-class DebugPasswordResetForm(PasswordResetForm):
-    def save(self, *args, **kwargs):
-        # Log the email from the form
-        email = self.cleaned_data.get('email')
-        users = list(self.get_users(email))
-        logger.debug("Password reset requested for email: %s", email)
-        logger.debug("Found %d user(s) for that email.", len(users))
-        # Continue with the normal save, which sends the email if users exist.
-        return super().save(*args, **kwargs)
 class CustomErrorList(ErrorList):
     def __str__(self):
         if not self:
@@ -22,10 +11,20 @@ class CustomErrorList(ErrorList):
         return mark_safe(''.join([
             f'<div class="alert alert-danger" role="alert">{e}</div>' for e in self]))
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control'}),
+        label="Email Address"
+    )
+
+    class Meta:
+        model = User;
+        # Include 'email' in the fields list
+        fields = ("username", "email", "password1", "password2")
+
     def __init__(self, *args, **kwargs):
         super(CustomUserCreationForm, self).__init__ (*args, **kwargs)
-        for fieldname in ['username', 'password1',
-        'password2']:
+        for fieldname in ['username', 'email', 'password1','password2']:
             self.fields[fieldname].help_text = None
             self.fields[fieldname].widget.attrs.update(
                 {'class': 'form-control'}
