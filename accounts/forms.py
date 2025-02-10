3198ec8fef0d31058f8e01f79c08462a749a7699
Hypersomnia1 <harryfei04@gmail.com>
2025-02-10 03:37:34 -0500

    reset password.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,20 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.utils import ErrorList
 from django.utils.safestring import mark_safe
+from django.contrib.auth.forms import PasswordResetForm
+import logging
+
+logger = logging.getLogger(__name__)
+
+class DebugPasswordResetForm(PasswordResetForm):
+    def save(self, *args, **kwargs):
+        # Log the email from the form
+        email = self.cleaned_data.get('email')
+        users = list(self.get_users(email))
+        logger.debug("Password reset requested for email: %s", email)
+        logger.debug("Found %d user(s) for that email.", len(users))
+        # Continue with the normal save, which sends the email if users exist.
+        return super().save(*args, **kwargs)
 class CustomErrorList(ErrorList):
     def __str__(self):
         if not self:
